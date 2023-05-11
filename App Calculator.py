9a2bc200048c0c5bd38b102638c30c5360840f23
rsevial <rebekahsevial@gmail.com>
2023-05-12 07:47:01 +0800

    Create Pseudocode

---
+++ App Calculator.py
@@ -0,0 +1,35 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Assignment 4
+# App Calcuator
+
+# Print Simple Calculator
+# Display the main menu of the calculator using user-defined functions
+
+# Use while loop
+# Ask the user to decide which operator to used
+# Use the try and except function to check error when asking what operator to use
+# Add condition when the user inputted an invalid number
+
+# Ask the user for the two integers then assign them into variables 
+# Call the add function if the user choose 1
+# Call the subtract function if the user choose 2
+# Call the multiply function if the user choose 3
+# Call the divide function if the user choose 4 
+
+# Use the except ValueError and TypeError if the user entered the wrong values
+
+# Use an else statement if the user entered an integer greater than 4
+# Use the except ValueError and TypeError if the user entered the wrong values
+
+# Define a function which is the sum, which will add the two inputted numbers
+# Define a function which is the subtraction, which will subtract the two inputted numbers
+# Define a function which is the multiplication, which will multiply the two inputted numbers
+# Define a function which is the divison, which will divide the two inputted numbers
+# Use the except ZeroDivisionError if the user divided by zero
+# Call for the function display menu
+
+# While loop for the program to continue unless the user stops it
+# Ask the user if he/she wants to continue
+# Continue the program
+# Stop the program
+# If what the user inputted is invalid then he/she will be asked to try again
